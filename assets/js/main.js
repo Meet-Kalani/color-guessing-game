f52decbdbee7763cf61cb4c9205c04c3f2404be9
Meet Kalani <meetkalani2002@gmail.com>
2024-02-26 11:29:44 +0530

    suggested changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,107 @@
 // DOM elements
-const optionsElement = document.querySelector('.options');
-const messageElement = document.querySelector('.message');
-const colorBoxElement = document.querySelector('.color-box');
-const blackBtnElement = document.querySelector('.black-btn');
-const brownBtnElement = document.querySelector('.brown-btn');
-const resetBtnElement = document.querySelector('.reset-btn');
-const scoreValueElement = document.querySelector('.score-value');
-const difficultyLevelElement = document.querySelector('.difficulty-level');
+const options = document.querySelector(".options");
+const message = document.querySelector(".message");
+const colorBox = document.querySelector(".color-box");
+const blackBtn = document.querySelector(".black-btn");
+const brownBtn = document.querySelector(".brown-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const scoreValue = document.querySelector(".score-value");
+const difficultyLevel = document.querySelector(".difficulty-level");
 
-let colors = ['Red', 'Green', 'Blue'];
+let colors = ["Red", "Green", "Blue"];
 let score = 0;
-let difficulty = 'easy';
+let difficulty = "easy";
 
-resetBtnElement.addEventListener('click', resetGame);
-difficultyLevelElement.addEventListener('change',setDifficulty);
+resetBtn.addEventListener("click", resetGame);
+difficultyLevel.addEventListener("change", setDifficulty);
 
 // function handles user guesses in the game, updates the score and feedback message accordingly, and changes the color of the color box to a randomly generated color.
-optionsElement.addEventListener('click', (event) => {
-    const btn = event.target;
-    let color = generateRandomColor();
-    let guess = btn.textContent;
-    if (color === guess) {
-        score += getScoreIncrement();
-        messageElement.textContent = 'Correct!';
-    } else {
-        messageElement.textContent = 'Wrong!';
-    }
-    scoreValueElement.textContent = score;
+options.addEventListener("click", (event) => {
+  const btn = event.target;
+  const color = generateRandomColor();
+  const guess = btn.innerText;
 
-    setColorBoxColor(color);
-})
+  if (color === guess) {
+    score += getScoreIncrement();
+    message.innerText = "Correct!";
+  } else {
+    message.innerText = "Wrong!";
+  }
+  scoreValue.innerText = score;
+
+  setColorBoxColor(color);
+});
 
 function setDifficulty() {
-    difficulty = difficultyLevelElement.value;
+  difficulty = difficultyLevel.value;
 
-    if (difficulty === 'medium') {
-        colors.push('Black')
-        blackBtnElement.style.display = "initial";
-        brownBtnElement.style.display = "none";
-    } else if (difficulty === 'hard') {
-        colors.push('Black', 'Brown')
-        brownBtnElement.style.display = blackBtnElement.style.display = "initial";
-    } else {
-        brownBtnElement.style.display = blackBtnElement.style.display = "none";
+  if (difficulty === "medium") {
+    removeFromColors("Brown");
+    addToColorsIfNotExists("Black");
+    btnVisibilityToggler(brownBtn, "none");
+    btnVisibilityToggler(blackBtn, "initial");
+  } else if (difficulty === "hard") {
+    removeFromColors("Black");
+    addToColorsIfNotExists("Black", "Brown");
+    btnVisibilityToggler(brownBtn, "initial");
+    btnVisibilityToggler(blackBtn, "initial");
+  } else {
+    removeFromColors("Brown", "Black");
+    btnVisibilityToggler(brownBtn, "none");
+    btnVisibilityToggler(blackBtn, "none");
+  }
+  resetGame();
+}
+
+function removeFromColors(...colorValues) {
+  colorValues.forEach((colorValue) => {
+    let index = colors.indexOf(colorValue);
+    if (index !== -1) {
+      colors.splice(index, 1);
     }
+  });
+}
 
-    resetGame();
+function addToColorsIfNotExists(...colorValues) {
+  colorValues.forEach((colorValue) => {
+    if (!colors.includes(colorValue)) {
+      colors.push(colorValue);
+    }
+  });
+}
+
+function btnVisibilityToggler(element, propertyValue) {
+  if (propertyValue === "none") {
+    element.classList.add("d-none");
+  } else {
+    element.classList.remove("d-none");
+  }
 }
 
 function resetGame() {
-    scoreValueElement.textContent = score = 0;
-    messageElement.textContent = 'Welcome!';
-    setColorBoxColor('white');
+  scoreValue.innerText = score = 0;
+  message.innerText = "Welcome!";
+  setColorBoxColor("white");
 }
 
 function generateRandomColor() {
-    let randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
 }
 
 function getScoreIncrement() {
-    switch (difficulty) {
-        case 'easy':
-            return 1;
-        case 'medium':
-            return 2;
-        case 'hard':
-            return 3;
-        default:
-            return 1;
-    }
+  switch (difficulty) {
+    case "easy":
+      return 1;
+    case "medium":
+      return 2;
+    case "hard":
+      return 3;
+    default:
+      return 1;
+  }
 }
 
 function setColorBoxColor(color) {
-    colorBoxElement.style.backgroundColor = color.toLowerCase();
+  colorBox.style.backgroundColor = color.toLowerCase();
 }
